40145acfe5c2299330036f0af885ad0026b5da5f
Camilo Lillo <camilolillo@Camilos-MacBook-Pro.local>
2025-12-19 02:20:32 -0300

    Created SourceTabBar

--- Pokedex/Views/Home/HomeView.swift
+++ Pokedex/Views/Home/HomeView.swift
@@ -4,6 +4,7 @@ struct HomeView: View {
 
     @StateObject var viewModel: HomeViewModel
     @State private var selectedPokemon: Pokemon?
+    @State private var selectedTab: SourceTab = .all
 
     var body: some View {
         ZStack {
@@ -12,7 +13,12 @@ struct HomeView: View {
 
             VStack(spacing: 12) {
                 header
-
+                SourceTabBar(
+                    selectedTab: selectedTab,
+                    onTabSelected: { tab in
+                        selectedTab = tab
+                    }
+                )
                 pokemonScrollView
                     .padding(.top, 12)
                     .padding(.horizontal, 24)
@@ -31,7 +37,21 @@ struct HomeView: View {
 }
 
 private extension HomeView {
+    func onSignOutButtonPressed() {
+        viewModel.onSignOutButtonPressed()
+    }
 
+    private var displayedItems: [PokemonItem] {
+        switch selectedTab {
+        case .all:
+            return viewModel.items
+        case .favorites:
+            return viewModel.items.filter { $0.favorite }
+        }
+    }
+}
+
+private extension HomeView {
     var header: some View {
         HStack {
             IsoLogo()
@@ -50,26 +70,16 @@ private extension HomeView {
         }
     }
 
-    func onSignOutButtonPressed() {
-        viewModel.onSignOutButtonPressed()
-    }
-}
-
-private extension HomeView {
-
     var pokemonScrollView: some View {
         ScrollView {
             LazyVStack(spacing: 12) {
-                ForEach(viewModel.items) { item in
+                ForEach(displayedItems) { item in
                     pokemonRow(item: item)
                 }
             }
         }
     }
-}
-
-private extension HomeView {
-
+    
     func pokemonRow(item: PokemonItem) -> some View {
         let rowData = PokemonItemRowDataModel(item: item)
 
